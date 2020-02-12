7ee1b5bad1481dbd5e6e0d50a8cfa8a7e716e782
Mikhail Yuzbashev <yuzbashev.m@gmail.com>
2020-02-12 23:42:15 +0300

    #003 - add empty FlyWay script, add some setting at application.yaml, configure embedded datasource use H2 driver

--- library-service/src/main/java/ru/simplelib/library/service/config/Datasource.java
+++ library-service/src/main/java/ru/simplelib/library/service/config/Datasource.java
@@ -1,11 +1,19 @@
 package ru.simplelib.library.service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJdbcRepositories
 public class Datasource {
-
-
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.H2).build();
+    }
 }
